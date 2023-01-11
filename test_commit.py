03d38c66da5fd91415c302c41fa9f8328ed8f432
alesiram <vasquem2@oregonstate.edu>
2023-01-11 11:16:08 -0800

    Test commit

---
+++ test_commit.py
@@ -0,0 +1,9 @@
+# Name: Marisela Vasquez
+# OSU Email: Vasquem2@oregonstate.edu
+# Course: CS361 - Software Engineering I
+# Assignment 1: Environment Setup
+# Due Date: 1/11/23
+# Description: Set up your development environment.
+# In addition to an IDE or code editor (choose any you prefer), start a GitHub repository and choose a task management system.
+
+
